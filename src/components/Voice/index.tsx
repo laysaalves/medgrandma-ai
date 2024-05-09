70a42bb7994e49f8d9946be6f0619d5a2a53ec31
Laysa <laysa.developer@gmail.com>
2024-05-09 15:00:22 -0300

    feat: add initial voice component

---
+++ src/components/Voice/index.tsx
@@ -0,0 +1,31 @@
+import { MaterialIcons } from '@expo/vector-icons';
+
+import { Text, View, TouchableOpacity } from 'react-native';
+
+import { styles } from './styles';
+
+type Props = {
+  message: string;
+}
+
+export function Voice({ message, ...rest }: Props) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+      activeOpacity={0.7}
+      style={styles.container}
+      {...rest}
+    >
+      <MaterialIcons
+        name="play-circle"
+        color="#000"
+        size={28}
+      />
+
+      <Text style={styles.message}>
+        {message}
+      </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
---
+++ src/components/Voice/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    gap: 12,
+    alignItems: 'center',
+    height: 56,
+    borderRadius: 50,
+    justifyContent: 'center',
+    backgroundColor: "#9FF9CC",
+  },
+  message: {
+    fontFamily: "Poppins_400Regular",
+    fontSize: 16,
+    color: "#000",
+  },
+});
